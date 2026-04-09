1bbbd0a87df4251f5b5ffb6761c78f04cb3155a4
Log Lab <testesappandroid@gmail.com>
2026-04-09 13:14:09 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import PoliticaPrivacidade from './pages/PoliticaPrivacidade';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
